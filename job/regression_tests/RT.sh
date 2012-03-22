c2f3506f861a473d1a6dc56b40d707ed064c2257
ratko.vasic@noaa.gov <ratko.vasic@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2012-03-22 16:20:30 +0000

    Trunk commit: fix RT.sh from previous commit.
    git-svn-id: https://svnemc.ncep.noaa.gov/projects/nems/trunk@18386 281fb692-3a18-0410-826d-9e847983f365

--- job/regression_tests/RT.sh
+++ job/regression_tests/RT.sh
@@ -50,8 +50,8 @@ fi
 # RTPWD - Path to previously stored regression test answers
 ############################################################
 
-#  export RTPWD=${DISKNM}/noscrub/wx20rv/REGRESSION_TEST
-   export RTPWD=${STMP}/${USER}/REGRESSION_TEST
+   export RTPWD=${DISKNM}/noscrub/wx20rv/REGRESSION_TEST
+#  export RTPWD=${STMP}/${USER}/REGRESSION_TEST
 
 #########################################################################
 # Check if running regression test or creating baselines.
