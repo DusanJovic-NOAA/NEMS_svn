7178874ee5f0b4498aacc3df6a65a74aff01b867
jun.wang@noaa.gov <jun.wang@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2014-04-22 19:51:37 +0000

    In NEMS, Current gsm digital filter applied digital filter to all the vertical levels. But in wam we would like to keep upper level tides untouched, which means no filtering should be applied above lower atmosphere. We need the flexibility to control the top level of DFI.
    https://svnemc.ncep.noaa.gov/trac/nems/ticket/85
    
    
     M      src/atmos
    X       src/atmos/gsm
    M       job/regression_tests/detect_machine.sh
    M       job/exglobal_fcst.sh.sms_nems
    M       job/run_gh_l150_32pe
    
    
    
    git-svn-id: https://svnemc.ncep.noaa.gov/projects/nems/trunk@39528 281fb692-3a18-0410-826d-9e847983f365

--- job/regression_tests/detect_machine.sh
+++ job/regression_tests/detect_machine.sh
@@ -1,44 +1,46 @@
+#!/bin/sh
 
 HOSTNAME='hostname -f' ; if [ `uname` = AIX ]; then HOSTNAME='hostname' ; fi
 
 case `$HOSTNAME` in
 
-  g10a1.ncep.noaa.gov)               MACHINE_ID=wcoss ;;   ### gyre 1
-  g10a2.ncep.noaa.gov)               MACHINE_ID=wcoss ;;   ### gyre 2
-  g14a1.ncep.noaa.gov)               MACHINE_ID=wcoss ;;   ### gyre 3
-  g14a2.ncep.noaa.gov)               MACHINE_ID=wcoss ;;   ### gyre 4
+  g10a1.ncep.noaa.gov)               MACHINE_ID=wcoss ;; ### gyre 1
+  g10a2.ncep.noaa.gov)               MACHINE_ID=wcoss ;; ### gyre 2
+  g14a1.ncep.noaa.gov)               MACHINE_ID=wcoss ;; ### gyre 3
+  g14a2.ncep.noaa.gov)               MACHINE_ID=wcoss ;; ### gyre 4
 
-  t10a1.ncep.noaa.gov)               MACHINE_ID=wcoss ;;   ### tide 1
-  t10a2.ncep.noaa.gov)               MACHINE_ID=wcoss ;;   ### tide 2
-  t14a1.ncep.noaa.gov)               MACHINE_ID=wcoss ;;   ### tide 3
-  t14a2.ncep.noaa.gov)               MACHINE_ID=wcoss ;;   ### tide 4
+  t10a1.ncep.noaa.gov)               MACHINE_ID=wcoss ;; ### tide 1
+  t10a2.ncep.noaa.gov)               MACHINE_ID=wcoss ;; ### tide 2
+  t14a1.ncep.noaa.gov)               MACHINE_ID=wcoss ;; ### tide 3
+  t14a2.ncep.noaa.gov)               MACHINE_ID=wcoss ;; ### tide 4
 
-  gaea1.ncrc.gov)                    MACHINE_ID=gaea ;;    ### gaea1
-  gaea2.ncrc.gov)                    MACHINE_ID=gaea ;;    ### gaea2
-  gaea3.ncrc.gov)                    MACHINE_ID=gaea ;;    ### gaea3
-  gaea4.ncrc.gov)                    MACHINE_ID=gaea ;;    ### gaea4
-  gaea5.ncrc.gov)                    MACHINE_ID=gaea ;;    ### gaea5
-  gaea6.ncrc.gov)                    MACHINE_ID=gaea ;;    ### gaea6
-  gaea7.ncrc.gov)                    MACHINE_ID=gaea ;;    ### gaea7
-  gaea8.ncrc.gov)                    MACHINE_ID=gaea ;;    ### gaea8
-  gaea9.ncrc.gov)                    MACHINE_ID=gaea ;;    ### gaea9
-  gaea10.ncrc.gov)                   MACHINE_ID=gaea ;;    ### gaea10
+  gaea1.ncrc.gov)                    MACHINE_ID=gaea ;; ### gaea1
+  gaea2.ncrc.gov)                    MACHINE_ID=gaea ;; ### gaea2
+  gaea3.ncrc.gov)                    MACHINE_ID=gaea ;; ### gaea3
+  gaea4.ncrc.gov)                    MACHINE_ID=gaea ;; ### gaea4
+  gaea5.ncrc.gov)                    MACHINE_ID=gaea ;; ### gaea5
+  gaea6.ncrc.gov)                    MACHINE_ID=gaea ;; ### gaea6
+  gaea7.ncrc.gov)                    MACHINE_ID=gaea ;; ### gaea7
+  gaea8.ncrc.gov)                    MACHINE_ID=gaea ;; ### gaea8
+  gaea9.ncrc.gov)                    MACHINE_ID=gaea ;; ### gaea9
+  gaea10.ncrc.gov)                   MACHINE_ID=gaea ;; ### gaea10
 
-  fe1.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;;    ### zeus1
-  fe2.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;;    ### zeus2
-  fe3.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;;    ### zeus3
-  fe4.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;;    ### zeus4
-  fe5.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;;    ### zeus5
-  fe6.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;;    ### zeus6
-  fe7.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;;    ### zeus7
-  fe8.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;;    ### zeus8
+  fe1.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;; ### zeus1
+  fe2.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;; ### zeus2
+  fe3.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;; ### zeus3
+  fe4.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;; ### zeus4
+  fe5.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;; ### zeus5
+  fe6.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;; ### zeus6
+  fe7.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;; ### zeus7
+  fe8.zeus.fairmont.rdhpcs.noaa.gov) MACHINE_ID=zeus ;; ### zeus8
 
 esac
 echo "Machine: " $MACHINE_ID "    Account: " $ACCNR
 
 # --- for Zeus, find available account ID
   AP=account_params          # Account info
-if [ ${MACHINE_ID} = zeus -a ${ACCNR:-null} = null ]; then
+if [ ${MACHINE_ID} = zeus  ]; then
+if [ ${ACCNR:-null} = null ]; then
 
   ac=`$AP 2>&1 | grep '^\s*Allocation: [0-9]' | awk '$4>100{print $3}'| head -1`
   nr=`echo $ac|wc -w`
@@ -62,4 +64,5 @@ else
   nr=`echo $cphr|wc -w`
   if [ $nr -eq 0 ]; then echo 'Wrong account choice: ' $ACCNR ; exit ; fi
   echo "Account: " $ACCNR", available: " $cphr " CPU hrs"
+fi
 fi
