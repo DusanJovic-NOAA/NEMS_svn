1dd4325ca6769c655fd2ea7e283477c885c2dfd1
jun.wang@noaa.gov <jun.wang@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2016-10-11 15:14:55 +0000

    Trunk commit, nems ticket 201: Add timers to the GSM code
     M      .
     M      tests
    M       tests/Compile_theia.log
    M       tests/Compile_wcoss.log
    M       tests/RegressionTests_theia.log
    M       tests/RegressionTests_wcoss.log
     M      src
     M      src/atmos
    D       src/atmos/share/module_TIMERS.F90
    M       src/atmos/share/makefile
    D       src/atmos/io
    D       src/atmos/io/module_GET_CONFIG_WRITE_GFS.F90
    D       src/atmos/io/module_WRITE_ROUTINES_GFS.F90
    D       src/atmos/io/module_WRITE_INTERNAL_STATE_GFS.F90
    D       src/atmos/io/module_GFS_WRITE.F90
    D       src/atmos/io/module_include_io.F90
    D       src/atmos/io/module_io_mpi_def.F90
    D       src/atmos/io/makefile
    D       src/atmos/io/module_WRITE_GRID_COMP_GFS.F90
    M       src/atmos/makefile
    M       src/makefile
    
    
    
    git-svn-id: https://svnemc.ncep.noaa.gov/projects/nems/trunk@82935 281fb692-3a18-0410-826d-9e847983f365

--- src/atmos/share/module_TIMERS.F90
+++
@@ -1,9 +0,0 @@
-    module module_timers
-      implicit none
-      public timef
-      contains
-      function timef()
-       real(kind=8):: timef,mpi_wtime
-       timef=MPI_Wtime()
-      end function timef
-    end module module_timers
